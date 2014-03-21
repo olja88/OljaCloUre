688ada39c9539c99dc05d3bc56db7960cb29ccdd
Olja Latinović <oljalatinovic88@gmail.com>
2014-03-21 21:08:51 +0100

    Ubaceni obicni stilovi, pre probe sa css3 i html5

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :min-lein-version "2.0.0"
   :license {:name "The MIT License"
             :url "https://raw.githubusercontent.com/olja88/OljaCloUre/master/LICENSE"}
-:dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.2"]
                  [postgresql "9.1-901.jdbc4"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [compojure "1.1.6"]
-                 [hiccup "1.0.4"]])
+                 [hiccup "1.0.4"]]
+  :main oljacl.core
+  :aot [oljacl.core])
