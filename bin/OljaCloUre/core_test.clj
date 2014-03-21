78c83bae1aa1a7d6d85b016fd663c469078ab7c8
Olja Latinović <oljalatinovic88@gmail.com>
2014-03-21 19:15:22 +0100

    Inicijalni commit

---
+++ bin/OljaCloUre/core_test.clj
@@ -0,0 +1,7 @@
+(ns OljaCloUre.core-test
+  (:require [clojure.test :refer :all]
+            [OljaCloUre.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to OljaCloUre
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
