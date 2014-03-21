c64fdff8c7d76065cd8f6b05ecc6fd95fbf77e04
Olja Latinović <oljalatinovic88@gmail.com>
2014-03-21 21:36:07 +0100

    deleted:    bin/OljaCloUre/core.clj 	deleted:    bin/OljaCloUre/core_test.clj 	deleted:    bin/OljaCloUre/view.clj 	deleted:    bin/oljacl/view/glavniView.clj 	deleted:    src/OljaCloUre/core.clj 	deleted:    src/OljaCloUre/view.clj 	deleted:    src/oljacl/view/glavniView.clj 	deleted:    test/OljaCloUre/core_test.clj

--- src/OljaCloUre/core.clj
+++
@@ -1,6 +0,0 @@
-(ns OljaCloUre.core)
-
-(defn foo
-  "I do not do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/OljaCloUre/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns OljaCloUre.core-test
-  (:require [clojure.test :refer :all]
-            [OljaCloUre.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
