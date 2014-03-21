5cb955ee1ad09dd5bfb2e74b2f910e0918583845
Olja Latinović <oljalatinovic88@gmail.com>
2014-03-21 19:41:22 +0100

    Dependencies za pg

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject OljaCloUre "0.1.0-SNAPSHOT"
+(defproject OljaCloUre "0.1.0"
   :description "Clojure app powered by Olja Latinović"
   :url "https://github.com/olja88/OljaCloUre"
+  :min-lein-version "2.0.0"
   :license {:name "The MIT License"
             :url "https://raw.githubusercontent.com/olja88/OljaCloUre/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]])
