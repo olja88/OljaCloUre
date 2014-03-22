3818dc6cf34ea1980f241fb5b59ae070387dc96a
Olja Latinović <oljalatinovic88@gmail.com>
2014-03-22 01:59:07 +0100

    Test conditional komentara u hiccup-u

--- src/oljacl/view/layout.clj
+++ src/oljacl/view/layout.clj
@@ -8,11 +8,17 @@
     [:meta {:charset "utf-8"}]
     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
+    [:meta {:name "author" :content "Olja Latinović"}]
+    [:meta {:name "keywords" :content "Olja Latinović, Clojure"}]
     [:title title]
-    (include-css "/stylesheets/base.css"
-                 "/stylesheets/skeleton.css"
-                 "/stylesheets/screen.css")
-    (include-css "http://fonts.googleapis.com/css?family=Sigmar+One&v1")]
+    (include-css "/css/bootstrap.min.css")
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->    
+    ]
    [:body
     [:header {:id "header"}
     [:h1 {:class "container"} "Olja Clojure"]]
