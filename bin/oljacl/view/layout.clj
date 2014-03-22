d35a4feae0a5e8d23c1562f9b95b9016da391b3c
Olja Latinović <oljalatinovic88@gmail.com>
2014-03-22 04:03:58 +0100

    Test footera

--- bin/oljacl/view/layout.clj
+++ bin/oljacl/view/layout.clj
@@ -22,7 +22,8 @@
     [:h1 {:class "container"} "Olja Clojure"]]
     [:div {:id "content" :class "container"} body]
    
-   [:footer "&copy; Olja Latinović - 2014. "]
+   [:footer{:id "container"}
+           [:small "&copy; Olja Latinović - 2014. "]]
     ;;(include-js "/js/jquery-2.1.0.min.js")
     ;;(include-js "/js/bootstrap.min.js")      
    ]
