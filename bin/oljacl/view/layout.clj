907b38fb523a0bdf9ae24da98b67ddf18bbf91b1
Olja Latinović <oljalatinovic88@gmail.com>
2014-03-22 03:45:46 +0100

    css

--- bin/oljacl/view/layout.clj
+++ bin/oljacl/view/layout.clj
@@ -22,7 +22,7 @@
     [:h1 {:class "container"} "Olja Clojure"]]
     [:div {:id "content" :class "container"} body]
    
-   [:footer{:id "footer"}
+   [:footer{:id "container"}
            [:small "&copy; Olja Latinović - 2014. "]]
     ;;(include-js "/js/jquery-2.1.0.min.js")
     ;;(include-js "/js/bootstrap.min.js")      
